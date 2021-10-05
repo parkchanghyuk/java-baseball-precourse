4dedfb53800573056194f50493f3a5ccb11a7cee
박창혁 <changchang73@pionnet.co.kr>
2021-10-06 01:41:32 +0900

    feat: view 생성
    user view 화면 추가

---
+++ src/main/java/baseball/view/UserView.java
@@ -0,0 +1,37 @@
+﻿package baseball.view;
+
+import nextstep.utils.Console;
+
+/**
+ * @packageName : baseball.view
+ * @date : 2021-10-04
+ * @author : 박창혁
+ * @description :
+ **/
+
+public class UserView {
+	/**
+	 * @date : 2021-10-05
+	 * @author : 박창혁
+	 * @description : 공지
+	 **/
+	public void comment(String msg) {
+		System.out.println(msg);
+	}
+
+	/**
+	 * @date : 2021-10-05
+	 * @author : 박창혁
+	 * @description : 사용자 input 받기
+	 **/
+	public String readLine() {
+		return readLine("");
+	}
+
+	public String readLine(String msg) {
+		if (!msg.isEmpty()) {
+			System.out.print("");
+		}
+		return Console.readLine();
+	}
+}
