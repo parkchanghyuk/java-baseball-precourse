12e76432744fcecee2403367c06f41fadccc6f73
박창혁 <changchang73@pionnet.co.kr>
2021-10-06 01:18:38 +0900

    feat: 야구게임 aplication
    야구게임 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,9 @@
 package baseball;
 
+import baseball.controller.BaseballGameController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
-        // intellij commit test
-    }
+	public static void main(String[] args) {
+		new BaseballGameController().baseballGame();
+	}
 }
