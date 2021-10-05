743859073da897263e0dcfa3b40def531e1a7d72
박창혁 <changchang73@pionnet.co.kr>
2021-10-06 01:18:02 +0900

    feat: MESSAGE 공통 처리
    1. 일반 MSG 생성
    2. ERROR MSG 생성

---
+++ src/main/java/baseball/common/ErrMsgEnum.java
@@ -0,0 +1,22 @@
+﻿package baseball.common;
+
+/**
+ * @date : 2021-10-04
+ * @author : 박창혁
+ * @description : 오류 메시지 
+ **/
+public enum ErrMsgEnum {
+	SIZE("3자리의 숫자만 입력 가능합니다."),
+	RANGE(Const.START_NUM + "~" + Const.END_NUM + "까지의 숫자만 입력 가능합니다."),
+	NON_DUPLICATE_NUMBERS("숫자가 중복되었습니다.");
+
+	String msg;
+
+	ErrMsgEnum(String msg) {
+		this.msg = msg;
+	}
+
+	public String getMsg() {
+		return this.msg;
+	}
+}
---
+++ src/main/java/baseball/common/MsgEnum.java
@@ -0,0 +1,24 @@
+﻿package baseball.common;
+
+/**
+ * @date : 2021-10-04
+ * @author : 박창혁
+ * @description : 공통 메시지
+ **/
+public enum MsgEnum {
+	READY("게임에 참가를 원하십니까? 참가를 원하시면 1번을, 아니면 2번을 눌러주세요."),
+	WELCOME("이 자리에 오신 여러분을 진심으로 환영합니다."),
+	ING("1~9까지의 숫자를 입력해주세요."),
+	GAME_OVER("게임이 종료되었습니다.");
+
+	String msg;
+
+	MsgEnum(String msg) {
+		this.msg = msg;
+	}
+
+	public String getMsg() {
+		return this.msg;
+	}
+
+}
