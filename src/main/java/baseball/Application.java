322cb0473a8592874c74d2a724991a22dfb0dbe1
박창혁 <changchang73@pionnet.co.kr>
2021-10-06 16:49:26 +0900

    chore: 주석 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import baseball.controller.BaseballGameController;
 
 public class Application {
 	public static void main(String[] args) {
+		// 야구게임 시작
 		new BaseballGameController().baseballGame();
 	}
 }
